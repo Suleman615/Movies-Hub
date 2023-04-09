73012eccc4b34ee92f9d6b768ec709406e79e015
Ghulam Suleman <123741870+Suleman615@users.noreply.github.com>
2023-04-09 17:14:48 +0500

    Add files via upload

---
+++ movies.js
@@ -0,0 +1,44 @@
+let source=[{"id":1,"name":"Free Guy","img_url":"https://www.themoviedb.org/t/p/w220_and_h330_face/xmbU4JTUm8rsdtn7Y3Fcm30GpeT.jpg","url":"https://www.themoviedb.org/movie/550988","launch_date":"2021-08-11","rating":79,"createdAt":"2021-12-18T08:38:13.000Z","updatedAt":"2021-12-18T08:38:13.000Z"},{"id":2,"name":"Old","img_url":"https://www.themoviedb.org/t/p/w220_and_h330_face/vclShucpUmPhdAOmKgf3B3Z4POD.jpg","url":"https://www.themoviedb.org/movie/631843","launch_date":"2021-07-21","rating":67,"createdAt":"2021-12-18T08:38:13.000Z","updatedAt":"2021-12-18T08:38:13.000Z"},{"id":3,"name":"Shang-Chi and the Legend of the Ten Rings","img_url":"https://www.themoviedb.org/t/p/w220_and_h330_face/xeItgLK9qcafxbd8kYgv7XnMEog.jpg","url":"https://www.themoviedb.org/movie/566525","launch_date":"2021-09-01","rating":78,"createdAt":"2021-12-18T08:38:13.000Z","updatedAt":"2021-12-18T08:38:13.000Z"},{"id":4,"name":"The Suicide Squad","img_url":"https://www.themoviedb.org/t/p/w220_and_h330_face/kb4s0ML0iVZlG6wAKbbs9NAm6X.jpg","url":"https://www.themoviedb.org/movie/436969","launch_date":"2021-07-28","rating":79,"createdAt":"2021-12-18T08:38:13.000Z","updatedAt":"2021-12-18T08:38:13.000Z"},{"id":5,"name":"Catch the Bullet","img_url":"https://www.themoviedb.org/t/p/w220_and_h330_face/qKxrBZ8Ts4KHZKp7BT6GAVMLFO2.jpg","url":"https://www.themoviedb.org/movie/859860","launch_date":"2021-09-10","rating":61,"createdAt":"2021-12-18T08:38:13.000Z","updatedAt":"2021-12-18T08:38:13.000Z"},{"id":6,"name":"Snake Eyes: G.I. Joe Origins","img_url":"https://www.themoviedb.org/t/p/w220_and_h330_face/uIXF0sQGXOxQhbaEaKOi2VYlIL0.jpg","url":"https://www.themoviedb.org/movie/568620","launch_date":"2021-07-22","rating":70,"createdAt":"2021-12-18T08:38:13.000Z","updatedAt":"2021-12-18T08:38:13.000Z"},{"id":7,"name":"The Stronghold","img_url":"https://www.themoviedb.org/t/p/w220_and_h330_face/nLanxl7Xhfbd5s8FxPy8jWZw4rv.jpg","url":"https://www.themoviedb.org/movie/637534","launch_date":"2021-08-18","rating":79,"createdAt":"2021-12-18T08:38:13.000Z","updatedAt":"2021-12-18T08:38:13.000Z"},{"id":8,"name":"Kate","img_url":"https://www.themoviedb.org/t/p/w220_and_h330_face/uJgdT1boTSP0dDIjdTgGleg71l4.jpg","url":"https://www.themoviedb.org/movie/597891","launch_date":"2021-09-10","rating":68,"createdAt":"2021-12-18T08:38:13.000Z","updatedAt":"2021-12-18T08:38:13.000Z"},{"id":9,"name":"SAS: Red Notice","img_url":"https://www.themoviedb.org/t/p/w220_and_h330_face/6Y9fl8tD1xtyUrOHV2MkCYTpzgi.jpg","url":"https://www.themoviedb.org/movie/595743","launch_date":"2021-08-11","rating":60,"createdAt":"2021-12-18T08:38:13.000Z","updatedAt":"2021-12-18T08:38:13.000Z"},{"id":10,"name":"Black Widow","img_url":"https://www.themoviedb.org/t/p/w220_and_h330_face/qAZ0pzat24kLdO3o8ejmbLxyOac.jpg","url":"https://www.themoviedb.org/movie/497698","launch_date":"2021-07-07","rating":77,"createdAt":"2021-12-18T08:38:13.000Z","updatedAt":"2021-12-18T08:38:13.000Z"},{"id":11,"name":"Jungle Cruise","img_url":"https://www.themoviedb.org/t/p/w220_and_h330_face/9dKCd55IuTT5QRs989m9Qlb7d2B.jpg","url":"https://www.themoviedb.org/movie/451048","launch_date":"2021-07-28","rating":78,"createdAt":"2021-12-18T08:38:13.000Z","updatedAt":"2021-12-18T08:38:13.000Z"},{"id":12,"name":"Sweet Girl","img_url":"https://www.themoviedb.org/t/p/w220_and_h330_face/cP7odDzzFBD9ycxj2laTeFWGLjD.jpg","url":"https://www.themoviedb.org/movie/619297","launch_date":"2021-08-18","rating":69,"createdAt":"2021-12-18T08:38:13.000Z","updatedAt":"2021-12-18T08:38:13.000Z"},{"id":13,"name":"Jurassic Hunt","img_url":"https://www.themoviedb.org/t/p/w220_and_h330_face/bZnOioDq1ldaxKfUoj3DenHU7mp.jpg","url":"https://www.themoviedb.org/movie/848278","launch_date":"2021-09-01","rating":51,"createdAt":"2021-12-18T08:38:13.000Z","updatedAt":"2021-12-18T08:38:13.000Z"},{"id":14,"name":"F9","img_url":"https://www.themoviedb.org/t/p/w220_and_h330_face/bOFaAXmWWXC3Rbv4u4uM9ZSzRXP.jpg","url":"https://www.themoviedb.org/movie/385128","launch_date":"2021-05-19","rating":75,"createdAt":"2021-12-18T08:38:13.000Z","updatedAt":"2021-12-18T08:38:13.000Z"},{"id":15,"name":"PAW Patrol: The Movie","img_url":"https://www.themoviedb.org/t/p/w220_and_h330_face/ic0intvXZSfBlYPIvWXpU1ivUCO.jpg","url":"https://www.themoviedb.org/movie/675445","launch_date":"2021-08-09","rating":79,"createdAt":"2021-12-18T08:38:13.000Z","updatedAt":"2021-12-18T08:38:13.000Z"},{"id":16,"name":"The Last Warrior: Root of Evil","img_url":"https://www.themoviedb.org/t/p/w220_and_h330_face/5VJSIAhSn4qUsg5nOj4MhQhF5wQ.jpg","url":"https://www.themoviedb.org/movie/589754","launch_date":"2021-01-01","rating":76,"createdAt":"2021-12-18T08:38:13.000Z","updatedAt":"2021-12-18T08:38:13.000Z"},{"id":17,"name":"Escape Room: Tournament of Champions","img_url":"https://www.themoviedb.org/t/p/w220_and_h330_face/jGYJyPzVgrVV2bgClI9uvEZgVLE.jpg","url":"https://www.themoviedb.org/movie/585216","launch_date":"2021-07-01","rating":71,"createdAt":"2021-12-18T08:38:13.000Z","updatedAt":"2021-12-18T08:38:13.000Z"},{"id":18,"name":"Luca","img_url":"https://www.themoviedb.org/t/p/w220_and_h330_face/jTswp6KyDYKtvC52GbHagrZbGvD.jpg","url":"https://www.themoviedb.org/movie/508943","launch_date":"2021-06-17","rating":80,"createdAt":"2021-12-18T08:38:13.000Z","updatedAt":"2021-12-18T08:38:13.000Z"},{"id":19,"name":"The Boss Baby: Family Business","img_url":"https://www.themoviedb.org/t/p/w220_and_h330_face/uWStkK8bq9ixY3fc7y209ZleCoF.jpg","url":"https://www.themoviedb.org/movie/459151","launch_date":"2021-07-01","rating":78,"createdAt":"2021-12-18T08:38:13.000Z","updatedAt":"2021-12-18T08:38:13.000Z"},{"id":20,"name":"Space Jam: A New Legacy","img_url":"https://www.themoviedb.org/t/p/w220_and_h330_face/5bFK5d3mVTAvBCXi5NPWH0tYjKl.jpg","url":"https://www.themoviedb.org/movie/379686","launch_date":"2021-07-08","rating":74,"createdAt":"2021-12-18T08:38:13.000Z","updatedAt":"2021-12-18T08:38:13.000Z"}];
+
+function search(){
+    
+  
+    $.ajax({
+        url: "https://movieto-api.vercel.app/movie/all?page=1",
+        method : "GET",
+        datatype : "json",
+        success:  function(data){
+  
+        },
+        error : function(error){
+            console.log("error is = "+ error)
+        }
+    })
+
+}
+
+
+
+
+
+
+for(i=0; i<20;i++){
+
+ console.log(source[i])
+
+  const el= `<div class="my-lg-5 mx-2 border border-4 border-secondary rounded-3" style="width: fit-content;">
+          
+  <img src="${source[i].img_url}" alt="" >
+  <div style=" height: 180px; width: 220px;" class="d-flex flex-column justify-content-between   bg-secondary ">
+      <h4 class="p-1 m-0 text-center text-wrap"><b>${source[i].name}</b></h4>
+      <h4 class="text-center"><b>${source[i].rating}</b></h4>
+      <a class="align-self-center w-70     " href="${source[i].url}"><button class=" mb-2 px-4 py-1 btn-primary text-black   rounded-pill  " >Watch Now</button></a>
+  </div>
+  </div>`;
+  const node = document.createElement("div");
+  
+  node.innerHTML=el;
+  const element =document.getElementById("movies");
+  console.log(element)
+  element.appendChild(node);
+  }
